f8cb9549a29405c701fe307a1f87cf161bf1795a
jaji oluwatobiloba <jajmantobi@gmail.com>
2023-07-10 22:43:10 -0400

    Update resume

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -50,13 +50,13 @@ function AppBanner() {
 				>
 					<a
 						download="aikulola-resume.pdf"
-						href="/files/Stoman-Resume.pdf"
+						href="/files/aikulola-resume.pdf"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
 						<FiArrowDownCircle className="ml-0 sm:ml-1 mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
 						<span className="text-sm sm:text-lg duration-100">
-							Download CV
+							Download Resume
 						</span>
 					</a>
 				</motion.div>
